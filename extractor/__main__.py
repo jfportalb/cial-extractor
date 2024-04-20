7b44b20df4aafc2f9ea07f2817d12d1da25bf36e
João Albuquerque <joao.albuquerque@stoneage.com.br>
2024-04-20 12:12:58 -0300

    Parsing input concurrently and printing json

--- extractor/__main__.py
+++ extractor/__main__.py
@@ -1,9 +1,26 @@
 import fileinput
+import concurrent.futures as cf
+import json
+
+def parse_url(website):
+    logo = "<LOGO>"
+    phones = ["+61 3 9828 3200", "+55 11 4933 7500"]
+    return json.dumps({
+        "logo": logo,
+        "phones": phones,
+        "website": website
+    })
 
 def main():
-    for line in fileinput.input():
-        line = line.rstrip()
-        print(line)
+    with cf.ThreadPoolExecutor(max_workers=5) as executor:
+        future_to_url = {executor.submit(parse_url, url.rstrip()): url.rstrip() for url in fileinput.input()}
+        for future in cf.as_completed(future_to_url):
+            try:
+                json = future.result() 
+                print(json)
+            except Exception as exc:
+                url = future_to_url[future]
+                print('%r generated an exception: %s' % (url, exc))
 
 
 if __name__ == "__main__":
